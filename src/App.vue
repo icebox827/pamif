3f181a80849575b3b0937712336f4f7c7f855174
Denis Lafontant <icebox827@gmail.com>
2021-08-26 19:36:51 -0400

    Update link

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/contact">Contact</router-link>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container-fluid">
+         <a class="navbar-brand" href="#">
+            <!-- <img alt="pamif logo" src="../assets/logo_pamif.png"> -->
+          </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/about">About</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link active" to="/contact">Contact</router-link>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <a href="https://learn.cowater-pamifpnh.com" target="_blank">Plateforme Éducative</a>
+            <!-- <router-link class="nav-link active" to="/contact">Contact</router-link> -->
+          </form>
+        </div>
+      </div>
+    </nav>
   </div>
   <router-view/>
 </template>
