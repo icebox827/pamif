b2975fdcbc5e1e60c6cb4f5e83260031a137f76b
Denis Lafontant <icebox827@gmail.com>
2021-08-27 15:56:04 -0400

    Create footer component

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,18 @@
     </nav>
   </div>
   <router-view/>
+  <Footer />
 </template>
+
+<script>
+import Footer from '@/components/Footer.vue'
+
+export default {
+  components: {
+    Footer
+  }
+}
+</script>
 
 <style>
 * {
---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+   <footer>
+    <div class="footer d-flex">
+      <p> &copy; Built with love by Deni Lafontant</p>
+      <p>All right reserved</p>
+    </div>
+  </footer>
+</template>
