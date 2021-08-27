36dd93e73d22536d535f16677e1de843417e8a26
Denis Lafontant <icebox827@gmail.com>
2021-08-27 16:02:30 -0400

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,23 @@
 <template>
    <footer>
-    <div class="footer d-flex">
-      <p> &copy; Built with love by Deni Lafontant</p>
-      <p>All right reserved</p>
+    <div class="footer">
+      <p> &copy; Built with love by Denis Lafontant</p>
+      <p> 2021 - All rights reserved</p>
     </div>
   </footer>
 </template>
+
+<style>
+footer {
+  background-color: black;
+}
+.footer {
+  display: flex;
+  justify-content: center;
+}
+
+.footer p{
+  margin: 5px;
+  color: white;
+}
+</style>
