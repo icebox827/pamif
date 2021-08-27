cd332d4f8657a2430fedbc2ec18027ddb950dba0
Denis Lafontant <icebox827@gmail.com>
2021-08-27 17:20:16 -0400

    Integrate FA icon into APP

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
@@ -6,4 +7,7 @@ import router from './router'
 
 library.add(fas)
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .component('fa', FontAwesomeIcon)
+  .use(router)
+  .mount('#app')
