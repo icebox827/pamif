326b3887d9a56bb3af81ff905f10fa0c4a6c3734
Denis Lafontant <icebox827@gmail.com>
2021-08-28 10:57:21 -0400

    Add props to contact component

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,15 +1,34 @@
 <template>
   <section>
-    <form action="">
-      <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-      </div>
-      <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <h1>{{ msg }}</h1>
+    <div class="form">
+      <form action="">
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
   </section>
 </template>
+
+<script>
+export default {
+  name: 'ContactForm',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    margin-top: 15px;
+    text-align: center;
+  }
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="contact">
-    <ContactForm />
+    <ContactForm msg="Contactez-nous" />
   </div>
 </template>
 
