b6c59c04b20bb75b7610e8f1db44a9be361d67bd
Denis Lafontant <icebox827@gmail.com>
2021-08-28 20:08:07 -0400

    Update about view

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <div class="image" :style="{ backgroundImage: `url( ${Image} )` }">
+      <h1>{{ msg }}</h1>
+  </section>
+</template>
+
+<script>
+import Image from '@/assets/background2.png'
+
+export default {
+  data: () => ({
+    Image
+  }),
+  name: 'ContactForm',
+  props: {
+    msg: String
+  }
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <AboutPage msg="A propos" />
   </div>
 </template>
+
+<script>
+import AboutPage from '@/components/AboutPage.vue'
+
+export default {
+  name: 'about',
+  components: {
+    AboutPage
+  }
+}
+</script>
